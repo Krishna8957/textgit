df3b3d53cb2c49216b51a4abbcc7d0615b66b6d0
Krishna8957 <krishnapal7576@gmail.com>
2023-12-14 13:17:30 +0530

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({
 }));
 
 const productData = [];
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port ,() =>{
     console.log(`Connecting the server http://localhost:${port}`)
