0dc338eda87c168b239a71471e2fba6c5cfe06f4
Krishna8957 <krishnapal7576@gmail.com>
2023-12-14 13:10:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended : true,
+}));
+
+const productData = [];
+const port = 5000;
+
+app.listen(port ,() =>{
+    console.log(`Connecting the server http://localhost:${port}`)
+})
+
+// post api
+
+app.get("/",(req,res)=>{
+    res.send("hello this the homepage ")
+})
+
+app.post("/api/add_product",(req,res)=>{
+    console.log("Result",req.body);
+
+    const pdata = {
+        "id": productData.length+1,
+        "pname":req.body.pname,
+        "pprice":req.body.pprice,
+        "pdesc":req.body.pdesc,
+    };
+
+productData.push(pdata);
+console.log("final",pdata);
+
+res.status(200).send({
+    "status code": 200,
+    "message":"Product added successfully",
+    "product":pdata
+})
+
+});
+
+app.get("/api/get_product",(req,res)=>{
+
+    if(productData.length >0){
+      res.status(200).send({
+        "status_code": 200,
+        "product":productData
+
+      })
+    } else{
+   
+        res.status(200).send({
+            "status_code": 200,
+            "product":[]
+    
+          })
+    }
+})
